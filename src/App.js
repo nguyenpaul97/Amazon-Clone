05b4321265f2be8d6a6feb11d1b99994e2f3d995
nguyenpaul97 <nguyenpaul.ic.1997@gmail.com>
2020-10-30 13:11:26 -0700

    Built Amazon-Clone

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { useEffect } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './Header';
+import Home from './Home';
+import Checkout from './Checkout';
+import Login from './Login';
+import { useStateValue } from './StateProvider';
+import { auth } from './firebase';
+
+function App() {
+  const [{ user }, dispatch] = useStateValue();
+
+  // Piece of code which runs based on a given condition (useEffect Hook)
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // the user is logged in
+        dispatch({
+          type: 'SET_USER',
+          user: authUser,
+        });
+      } else {
+        // user is logged out
+        dispatch({
+          type: 'SET_USER',
+          user: null,
+        });
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/login">
+            <Header />
+            <Login />
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: 'AIzaSyAA9YCDTBfeQU9JHGVXtw_k3WRcHDrFYyc',
+  authDomain: 'clone-186ed.firebaseapp.com',
+  databaseURL: 'https://clone-186ed.firebaseio.com',
+  projectId: 'clone-186ed',
+  storageBucket: 'clone-186ed.appspot.com',
+  messagingSenderId: '642931510763',
+  appId: '1:642931510763:web:24c78ca3866ce55f6fddae',
+  measurementId: 'G-LEWVHR5TFZ',
+});
+
+const auth = firebase.auth();
+
+export { auth };
